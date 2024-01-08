2ed0a6945f6957b5e066cbff6a7daf4fbed91a00
marcosprofile <marcos_laurindo1997@hotmail.com>
2024-01-08 14:17:49 -0300

    coluna de ações

--- src/app/components/product/product-read/product-read.component.ts
+++ src/app/components/product/product-read/product-read.component.ts
@@ -3,19 +3,24 @@ import { AppMaterialModule } from '../../../app-material.module';
 import { Product } from '../product.model';
 import { ProductService } from '../product.service';
 import { CurrencyPipe } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 
 @Component({
   selector: 'app-product-read',
   standalone: true,
-  imports: [ AppMaterialModule, CurrencyPipe ],
+  imports: [
+    AppMaterialModule,
+    CurrencyPipe,
+    RouterLink
+  ],
   templateUrl: './product-read.component.html',
   styleUrl: './product-read.component.css'
 })
 
 export class ProductReadComponent implements OnInit {
   products: Product[] = []
-  displayedColumns = ['id', 'name', 'price'];
+  displayedColumns = ['id', 'name', 'price', 'action'];
 
   constructor(private productService: ProductService) {
 
