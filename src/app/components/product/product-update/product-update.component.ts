1c6760be96408baf32d12ecec7e20ce00da88121
marcosprofile <marcos_laurindo1997@hotmail.com>
2024-01-08 15:55:53 -0300

    Alterar produto

---
+++ src/app/components/product/product-update/product-update.component.ts
@@ -0,0 +1,47 @@
+import { Component, OnInit } from '@angular/core';
+import { AppMaterialModule } from '../../../app-material.module';
+import { ProductService } from '../product.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Product } from '../product.model';
+
+@Component({
+  selector: 'app-product-update',
+  standalone: true,
+  imports: [ AppMaterialModule ],
+  templateUrl: './product-update.component.html',
+  styleUrl: './product-update.component.css'
+})
+export class ProductUpdateComponent implements OnInit {
+
+  product: Product = {
+    name: '',
+    price: 0
+  };
+
+  constructor(
+    private productService: ProductService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id');
+
+    if (id !== null) {
+      this.productService.readById(id).subscribe(product => {
+        this.product = product;
+      })
+    }
+  }
+
+  updateProduct(): void {
+    this.productService.update(this.product).subscribe(() => {
+      this.productService.showMessage('Produto atualizado com sucesso!')
+      this.router.navigate(['/products'])
+    })
+  }
+
+  cancel(): void {
+    this.router.navigate(['/products'])
+  }
+}
